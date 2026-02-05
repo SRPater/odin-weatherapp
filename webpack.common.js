83b8358df32f45b385d3a8188b1d215e804bf9dd
Stefan Pater <srpater@gmail.com>
2026-02-05 11:50:41 +0100

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,33 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "New App",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
